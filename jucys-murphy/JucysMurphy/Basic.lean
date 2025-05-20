ef007270d383491774cc11cff12e4500d7ea1f29
Kristian Latvanen <kristianlatvanen@Kristian-MacBook-Air.local>
2025-05-20 10:59:09 +0300

    Fix sigma_sum_perm_eq

--- jucys-murphy/JucysMurphy/Basic.lean
+++ jucys-murphy/JucysMurphy/Basic.lean
@@ -9,88 +9,6 @@ abbrev S (n : ℕ) := Perm (Fin n)
 abbrev A (n : ℕ) := MonoidAlgebra ℂ (S n)
 
 
-noncomputable def jmElem (k n : ℕ) [NeZero n] : A n :=
-  ∑ i : Fin n with ↑i ∈ Finset.range (k - 1), MonoidAlgebra.of ℂ (S n) (swap i (k - 1))
-
-
-lemma σ_eq_comm {n : ℕ} [NeZero n] (σ : S n) (i : Fin n) (hσ : σ (n - 1) = n - 1) :
-    σ * (swap i (n-1)) = (swap (σ i) (n-1)) * σ := by
-  nth_rw 2 [hσ.symm]
-
-  rw [Perm.mul_def, Perm.mul_def]
-
-  rw [(Equiv.symm_trans_swap_trans i (n-1) σ).symm] -- Päälemma
-  rw [Equiv.trans_assoc, (Equiv.trans_assoc σ σ.symm (Equiv.trans (swap i (n-1)) σ)).symm] -- cancellaa sigmat
-  simp
-
-
--- TODO: Ei toimi nyt uudella S n määritelmällä: korjaa sitten ku kaikki määritelmät selvinny
-lemma σ_sum_perm_eq {n : ℕ} [NeZero n] : ∀ σ : (S n), (σ (n-1) = (n-1)) →
-    ∑ i : Fin n with ↑i ∈ Finset.range (n-1), MonoidAlgebra.of ℂ (S n) (swap (σ i) (n-1))
-      = ∑ i : Fin n with ↑i ∈ Finset.range (n-1), MonoidAlgebra.of ℂ (S n) (swap i (n-1)) := by
-
-  intro σ hσ
-
-  -- We want to use Finset.sum_equiv σ with the following f, g and of course σ.
-  let f : Fin (n+1) → MonoidAlgebra ℂ (S n) := fun k ↦ MonoidAlgebra.of ℂ (S n) (swap (σ k) n)
-  let g : Fin (n+1) → MonoidAlgebra ℂ (S n) := fun k ↦ MonoidAlgebra.of ℂ (S n) (swap k n)
-
-  have h_lt_iff : ∀ i : Fin n, i ∈ {i | ↑i ∈ Finset.range (n-1)} ↔ σ i ∈ {i : Fin n | ↑i ∈ Finset.range (n-1)} := by
-    intro i
-    constructor
-    · intro h
-      simp at h ⊢
-      apply @Fin.val_lt_last (n-1) (σ i)
-
-      by_contra h_eq
-      rw [hσ.symm] at h_eq
-      apply σ.injective at h_eq
-      rw [h_eq] at h
-      exact (lt_self_iff_false ↑(Fin.last n)).mp h
-    · intro h
-      simp_all
-      apply Fin.val_lt_last
-      by_contra h_eq
-      rw [h_eq] at h
-      rw [hσ] at h
-      exact (lt_self_iff_false ↑(Fin.last n)).mp h
-
-  have h_comp_eq : ∀ i ∈ {i : Fin (n+1) | ↑i ∈ Finset.range n}, f i = g (σ i) := by
-    unfold f g
-    intro i hi
-    rfl
-
-  -- Ei välttis tarvii mut sitä varten jos Lean ei tunnista f ja g implisiittisesti
-  -- En ymmärrä miks Finset.sum_equiv ei toimi?
-  have h_sum_eq : ∑ i ∈ {i : Fin n | ↑i ∈ Finset.range (n-1)}, (f i) = ∑ i ∈ {i : Fin n | ↑i ∈ Finset.range (n-1)}, (g i) := Finset.sum_equiv σ h_lt_iff h_comp_eq
-
-  unfold f g at h_sum_eq
-  exact h_sum_eq
-
-
-theorem jmElem_succ_comm_S_n' (n : ℕ) [NeZero n] (σ : S n) (hσ : σ (n - 1) = n - 1) :
-    Commute (jmElem n n) (MonoidAlgebra.of ℂ (S n) σ) := by
-  rw [jmElem, commute_iff_eq]
-
-  -- Distributivity
-  rw [Finset.mul_sum]
-
-  -- Coercion to MonoidAlgebra multiplicative
-  -- En tiiä miten rw summan sisällä mutta tää ainaki toimii
-  conv =>
-    enter [2, 2, i]
-    rw [(MonoidHom.map_mul (MonoidAlgebra.of ℂ (S n)) σ (swap i (↑n - 1))).symm]
-    rw [σ_eq_comm σ i hσ]
-    rw [MonoidHom.map_mul (MonoidAlgebra.of ℂ (S n)) (swap (σ i) (↑n - 1)) σ]
-
-  conv =>
-    rhs
-    rw [←Finset.sum_mul]
-
-  -- Use the reordering lemma
-  rw [σ_sum_perm_eq]
-  exact hσ
-
 
 -- Before `Utils` compiles:
 
@@ -122,6 +40,130 @@ def kth_lift_monAlg {n k : ℕ} [NeZero k]: A n →ₐ[ℂ] A (n + k) :=
   | 1 => lift_monAlg
   | k + 1 => lift_monAlg ∘ kth_lift_monAlg
 
+
+
+
+noncomputable def jmElem (k n : ℕ) [NeZero n] : A n :=
+  ∑ i : Fin n with ↑i ∈ Finset.range (k - 1), MonoidAlgebra.of ℂ (S n) (swap i (k - 1))
+
+
+lemma σ_swap_eq_swap_σ {n : ℕ} [NeZero n] (σ : S n) (i : Fin n) (hσ : σ (n - 1) = n - 1) :
+    σ * (swap i (n-1)) = (swap (σ i) (n-1)) * σ := by
+  nth_rw 2 [hσ.symm]
+
+  rw [Perm.mul_def, Perm.mul_def]
+
+  rw [(Equiv.symm_trans_swap_trans i (n-1) σ).symm] -- Päälemma
+  rw [Equiv.trans_assoc, (Equiv.trans_assoc σ σ.symm (Equiv.trans (swap i (n-1)) σ)).symm] -- cancellaa sigmat
+  simp
+
+
+
+lemma σ_sum_perm_eq {n : ℕ} [NeZero n] : ∀ σ : (S (n + 1)), (σ n = n) →
+    ∑ i : Fin (n + 1) with ↑i ∈ Finset.range n, MonoidAlgebra.of ℂ (S (n + 1)) (swap (σ i) n)
+      = ∑ i : Fin (n + 1) with ↑i ∈ Finset.range n, MonoidAlgebra.of ℂ (S (n + 1)) (swap i n) := by
+
+  intro σ hσ
+
+  -- We want to use Finset.sum_equiv σ with the following f, g and of course σ.
+  let f : Fin (n + 1) → MonoidAlgebra ℂ (S (n + 1)) := fun k ↦ MonoidAlgebra.of ℂ (S (n + 1)) (swap (σ k) n)
+  let g : Fin (n + 1) → MonoidAlgebra ℂ (S (n + 1)) := fun k ↦ MonoidAlgebra.of ℂ (S (n + 1)) (swap k n)
+
+  have h_lt_iff : ∀ i : Fin (n + 1), i ∈ {i | ↑i ∈ Finset.range n} ↔ σ i ∈ {i : Fin (n + 1) | ↑i ∈ Finset.range n} := by
+    intro i
+    constructor
+    · intro h
+      simp at h ⊢
+      apply @Fin.val_lt_last n (σ i)
+      rw [←Fin.natCast_eq_last n]
+
+      by_contra h_eq
+      rw [hσ.symm] at h_eq
+      apply σ.injective at h_eq
+      rw [h_eq] at h
+      simp at h
+    · intro h
+      simp_all
+      apply Fin.val_lt_last
+      by_contra h_eq
+      rw [h_eq] at h
+      rw [hσ] at h
+      simp at h
+
+  have h_comp_eq : ∀ i ∈ {i : Fin (n + 1) | ↑i ∈ Finset.range n}, f i = g (σ i) := by
+    unfold f g
+    intro i hi
+    rfl
+
+  -- Ei välttis tarvii mut sitä varten jos Lean ei tunnista f ja g implisiittisesti
+  -- En ymmärrä miks Finset.sum_equiv ei toimi?
+  have h_sum_eq : ∑ i ∈ {i : Fin (n + 1) | ↑i ∈ Finset.range n}, (f i) = ∑ i ∈ {i : Fin (n + 1) | ↑i ∈ Finset.range n}, (g i) := by
+    refine Finset.sum_equiv σ ?_ ?_
+    · simp at h_lt_iff ⊢
+      exact h_lt_iff
+    · simp at h_comp_eq ⊢
+      exact h_comp_eq
+
+  unfold f g at h_sum_eq
+  exact h_sum_eq
+
+
+-- TODO: joko todista tää tai korjaa vanha versio
+lemma σ_sum_perm_eq' {n : ℕ} [NeZero n] (σ : S n) :
+    ∑ i : Fin (n + 1) with ↑i ∈ Finset.range n, MonoidAlgebra.of ℂ (S (n + 1)) (swap (lift_perm σ i) n)
+      = ∑ i : Fin (n + 1) with ↑i ∈ Finset.range n, MonoidAlgebra.of ℂ (S (n + 1)) (swap i n) := by
+
+  have h_lt_iff : ∀ i : Fin (n + 1), i ∈ {i | ↑i ∈ Finset.range n} ↔ lift_perm σ i ∈ {i : Fin (n + 1) | ↑i ∈ Finset.range n} := by
+    intro i
+    unfold lift_perm
+    simp
+    rw [Perm.viaEmbeddingHom_apply]
+    constructor
+    · intro h_le
+      sorry
+    · by_contra h_eq
+      simp at h_eq
+      obtain ⟨h_leq, h_nleq⟩ := h_eq
+      have h_eq : n = ↑i := (Nat.eq_of_le_of_lt_succ h_nleq i.isLt).symm
+
+      rw [←h_eq] at h_leq
+
+
+      #check Perm.viaEmbedding_apply_of_not_mem σ Fin.castSuccEmb
+      sorry
+
+    --rw [Perm.viaEmbedding_apply_of_not_mem σ Fin.castSuccEmb i (by simp)]
+    --constructor
+    --· intro h_le
+
+
+  sorry
+
+
+
+
+theorem jmElem_succ_comm_S_n' (n : ℕ) [NeZero n] (σ : S (n + 1)) (hσ : σ n = n) :
+    Commute (jmElem (n + 1) (n + 1)) (MonoidAlgebra.of ℂ (S (n + 1)) σ) := by
+  rw [jmElem, commute_iff_eq]
+
+  -- Distributivity
+  rw [Finset.mul_sum]
+
+  -- Coercion to MonoidAlgebra multiplicative
+  -- En tiiä miten rw summan sisällä mutta tää ainaki toimii
+  conv =>
+    enter [2, 2, i]
+    rw [(MonoidHom.map_mul (MonoidAlgebra.of ℂ (S (n + 1))) σ (swap i (↑n))).symm]
+    rw [σ_swap_eq_swap_σ σ i hσ]
+    rw [MonoidHom.map_mul (MonoidAlgebra.of ℂ (S n)) (swap (σ i) (↑n - 1)) σ]
+
+  conv =>
+    rhs
+    rw [←Finset.sum_mul]
+
+  -- Use the reordering lemma
+  rw [σ_sum_perm_eq]
+  exact hσ
 
 
 -- TÄÄ MYÖS UTILS
